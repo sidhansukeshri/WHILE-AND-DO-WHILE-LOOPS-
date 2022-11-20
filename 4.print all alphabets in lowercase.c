cb5ac87e0f533c30e44adeda64723e7565e3c720
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-20 22:58:45 +0530

    Create 4.print all alphabets in lowercase.c

---
+++ 4.print all alphabets in lowercase.c
@@ -0,0 +1,13 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+int main()
+{
+	char alphabet;
+	alphabet='a';
+	printf("Uppercase alphabets:\n");
+	while(alphabet<='z')
+	{
+		printf("%c ",alphabet);
+		alphabet++;
+	}
+}
