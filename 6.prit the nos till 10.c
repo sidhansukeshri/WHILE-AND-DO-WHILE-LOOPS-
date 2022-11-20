bd01fa0a4341024cc76127fd9c8a7d14508d666b
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-20 23:02:46 +0530

    Create 6.prit the nos till 10.c

---
+++ 6.prit the nos till 10.c
@@ -0,0 +1,13 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+int main()
+{ 
+	int number;
+	number =1;
+	printf("Numbers from 1 to 10: \n");
+	while(number <= 10)
+	{
+		printf("%d\n",number);
+		number++;
+	}
+}
