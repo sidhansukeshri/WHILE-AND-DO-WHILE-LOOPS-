c2edfa32c8068a28f8732f324fd42a1d80cc731e
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-20 22:58:00 +0530

    Create 3.print all alphabets in uppercase.c

---
+++ 3.print all alphabets in uppercase.c
@@ -0,0 +1,13 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+int main()
+{
+	char alphabet;
+	alphabet='A';
+	printf("Uppercase alphabets:\n");
+	while(alphabet<='Z')
+	{
+		printf("%c ",alphabet);
+		alphabet++;
+	}
+}
